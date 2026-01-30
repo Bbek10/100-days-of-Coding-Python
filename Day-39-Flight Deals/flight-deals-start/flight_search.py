37800625301b2606333f0ffe7ec0a6f4780f544c
Bbek10 <bibekmanandhar2@gmail.com>
2026-01-30 20:46:05 +0545

    add: update_destination_codes implemented

--- Day-39-Flight Deals/flight-deals-start/flight_search.py
+++ Day-39-Flight Deals/flight-deals-start/flight_search.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations"
 FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
 TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
 
@@ -14,11 +14,9 @@ class FlightSearch:
         self._api_key = os.environ["AMADEUS_API_KEY"]
         self._api_secret = os.environ["AMADEUS_API_SECRET"]
         # Getting a new token every time program is run. Could reuse unexpired tokens as an extension.
-        #self._token = self._get_new_token()
+        self._token = self._get_new_token()
 
-    """
     def _get_new_token(self):
-        """
         #Generates the authentication token used for accessing the Amadeus API and returns it.
         #This function makes a POST request to the Amadeus token endpoint with the required
         #credentials (API key and API secret) to obtain a new client credentials token.
@@ -27,7 +25,7 @@ class FlightSearch:
         #    str: The new access token obtained from the API response.
         #         """
         # Header with content type as per Amadeus documentation
-    """"
+    
         header = {
             'content-type': 'application/x-www-form-urlencoded'
         }
@@ -42,8 +40,56 @@ class FlightSearch:
         print(f"Your token is {response.json()['access_token']}")
         print(f"Your token expires in {response.json()['expires_in']} seconds")
         return response.json()['access_token']      
-    """
+    
     def get_destination_code(self, city_name):
+  
         # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
-        code = "TESTING"
+        #code = "TESTING"
+        #return code
+        """
+        Retrieves the IATA code for a specified city using the Amadeus Location API.
+        Parameters:
+        city_name (str): The name of the city for which to find the IATA code.
+        Returns:
+        str: The IATA code of the first matching city if found; "N/A" if no match is found due to an IndexError, 
+        or "Not Found" if no match is found due to a KeyError.
+        
+        The function sends a GET request to the IATA_ENDPOINT with a query that specifies the city
+        name and other parameters to refine the search. It then attempts to extract the IATA code
+        from the JSON response. 
+        - If the city is not found in the response data (i.e., the data array is empty, leading to 
+        an IndexError), it logs a message indicating that no airport code was found for the city and 
+        returns "N/A".
+        - If the expected key is not found in the response (i.e., the 'iataCode' key is missing, leading 
+        to a KeyError), it logs a message indicating that no airport code was found for the city 
+        and returns "Not Found".
+        """
+        print(f"Using this token to get destination {self._token}")
+
+        headers = {
+            "Authorization": f"Bearer {self._token}"
+        }
+
+        query = {
+            "keyword": city_name,
+            "subType": "CITY",
+        }
+
+        response = requests.get(
+            url=IATA_ENDPOINT,
+            headers=headers,
+            params=query
+        )
+
+        print(f"Status code {response.status_code}. Airport IATA: {response.text}")
+        try:
+            code = response.json()["data"][0]["iataCode"]
+        except IndexError
+            print(f"No airport code found for {city_name}. Setting as N/A.")
+            code = "N/A"
+        except KeyError:
+            print(f"No airport code found for {city_name}. Setting as Not Found.")
+            code = "Not Found"
+
         return code
+       
--- Day-39-Flight Deals/flight-deals-start/main.py
+++ Day-39-Flight Deals/flight-deals-start/main.py
@@ -1,5 +1,10 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from flight_search import FlightSearch
+"""""
 
+FOLLOW STEPS STEP BY STEP
+
+"""
 # 4. Pass the data back to the main.py file, so that you can print the data from main.py
 from data_manager import DataManager
 #new instance of a class 
@@ -9,6 +14,7 @@ sheet_data = data_manager.get_destination_data()
 
 #print(sheet_data)
 update_data = data_manager.update_destination_codes()
+print(update_data)
 
 #  5. In main.py check if sheet_data contains any values for the "iataCode" key.
 #  If not, then the IATA Codes column is empty in the Google Sheet.
@@ -17,11 +23,11 @@ update_data = data_manager.update_destination_codes()
 #  for that city using the Flight Search API.
 #  You should use the code you get back to update the sheet_data dictionary.
 if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
     flight_search = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet_data:\n {sheet_data}")
+    #print(f"sheet_data:\n {sheet_data}")
 
     data_manager.destination_data = sheet_data
-    data_manager.update_destination_codes()
+    print(data_manager.update_destination_codes())
+
